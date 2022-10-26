4dcdcb62229a3d657dec5f79b94f62932acb6cce
gr0wth.eth <38635290+ethgr0wth@users.noreply.github.com>
2022-10-26 18:21:35 -0400

    Update IRECEIVE.sol

--- interfaces/IRECEIVE.sol
+++ interfaces/IRECEIVE.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.13;
 interface IRECEIVE {
     event Transfer(address indexed from, address indexed to, uint value);
 
-    function withdraw() external returns (bool);
     function withdrawETH() external returns (bool);
     function withdrawToken(address token) external returns (bool);
     function split(uint liquidity) external view returns(uint,uint,uint);
