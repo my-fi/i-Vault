6d3cc862c98785b4bd0fe3fdfeead8754540f636
gr0wth.eth <38635290+ethgr0wth@users.noreply.github.com>
2022-10-26 18:23:57 -0400

    Update and rename IERC20.sol to INTERFACES.sol

--- interfaces/INTERFACES.sol
+++ interfaces/INTERFACES.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
+import "../utils/Context.sol";
 
 interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint value);
@@ -14,4 +15,14 @@ interface IERC20 {
     function approve(address spender, uint value) external returns (bool);
     function transfer(address payable to, uint value) external returns (bool);
     function transferFrom(address payable from, address payable to, uint value) external returns (bool);
+}
+
+interface IVAULT {
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function withdrawETH() external returns (bool);
+    function withdrawToken(address token) external returns (bool);
+    function split(uint liquidity) external view returns(uint,uint,uint);
+    function transfer(address sender, uint256 eth, address payable receiver) external returns (bool success);
+    function transferToken(address sender, uint256 amount, address payable receiver, address token) external returns (bool success);
 }
