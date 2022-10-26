3db31cccf43de44c3d073beed5d2fba1f6337037
gr0wth.eth <38635290+ethgr0wth@users.noreply.github.com>
2022-10-26 18:33:19 -0400

    Update iVault.sol

--- iVault.sol
+++ iVault.sol
@@ -11,10 +11,6 @@ contract Vault is iAuth, IVAULT {
     string public symbol = unicode"🔑";
 
     mapping (address => uint8) public balanceOf;
-    mapping (address => uint) public coinAmountOwed;
-    mapping (address => uint) public coinAmountDrawn;
-    mapping (address => uint) public tokenAmountOwed;
-    mapping (address => uint) public tokenAmountDrawn;
 
     event Transfer(address indexed src, uint wad);
     event Withdrawal(address indexed src, uint wad);
@@ -22,28 +18,21 @@ contract Vault is iAuth, IVAULT {
     event TransferToken(address indexed src, address indexed token, uint wad);
  
     constructor() payable iAuth(address(_msgSender()),address(_Governor)) {
-        if(uint256(msg.value) > uint256(0)){
-            coinDeposit(uint256(msg.value));
-        }
     }
 
     receive() external payable {
         uint ETH_liquidity = msg.value;
         require(uint(ETH_liquidity) >= uint(0), "Not enough ether");
-        coinDeposit(uint256(ETH_liquidity));
     }
     
     fallback() external payable {
         uint ETH_liquidity = msg.value;
         require(uint(ETH_liquidity) >= uint(0), "Not enough ether");
-        coinDeposit(uint256(ETH_liquidity));
     }
 
     function setGovernor(address payable _developmentWallet) public authorized() returns(bool) {
         require(address(_Governor) == _msgSender());
         require(address(_Governor) != address(_developmentWallet),"!NEW");
-        coinAmountOwed[address(_developmentWallet)] += coinAmountOwed[address(_Governor)];
-        coinAmountOwed[address(_Governor)] = 0;
         _Governor = payable(_developmentWallet);
         (bool transferred) = transferAuthorization(address(_msgSender()), address(_developmentWallet));
         assert(transferred==true);
@@ -54,21 +43,9 @@ contract Vault is iAuth, IVAULT {
         return address(this).balance;
     }
 
-    function coinDeposit(uint256 amountETH) internal returns(bool) {
-        uint ETH_liquidity = amountETH;
-        return store(_msgSender(),uint(ETH_liquidity));
-    }
-
-    function store(address _depositor, uint eth_liquidity) internal returns(bool) {
-        coinAmountOwed[address(_depositor)] += uint(eth_liquidity);
-        return true;
-    }
-
     function withdrawETH() public authorized() returns(bool) {
         uint ETH_liquidity = uint(address(this).balance);
         assert(uint(ETH_liquidity) > uint(0));
-        coinAmountDrawn[address(_Governor)] += coinAmountOwed[address(_Governor)];
-        coinAmountOwed[address(_Governor)] = 0;
         payable(_Governor).transfer(ETH_liquidity);
         emit Withdrawal(address(this), ETH_liquidity);
         return true;
@@ -76,7 +53,6 @@ contract Vault is iAuth, IVAULT {
 
     function withdrawToken(address token) public authorized() returns(bool) {
         uint Token_liquidity = uint(IERC20(token).balanceOf(address(this)));
-        tokenAmountDrawn[address(_Governor)] += Token_liquidity;
         IERC20(token).transfer(payable(_Governor), Token_liquidity);
         emit WithdrawToken(address(this), address(token), Token_liquidity);
         return true;
@@ -92,9 +68,7 @@ contract Vault is iAuth, IVAULT {
             revert("!AUTH");
         }
         uint Eth_liquidity = address(this).balance;
-        coinAmountOwed[address(_Governor)] += uint(Eth_liquidity);
-        coinAmountDrawn[address(_Governor)] += uint(amount);
-        coinAmountOwed[address(_Governor)] -= uint(amount);
+        require(uint(amount) <= uint(Eth_liquidity),"Overdraft prevention: ETH");
         (bool successA,) = payable(_community_).call{value: amount}("");
         bool success = successA == true;
         assert(success);
@@ -113,9 +87,7 @@ contract Vault is iAuth, IVAULT {
         }
         bool success = false;
         uint Token_liquidity = IERC20(token).balanceOf(address(this));
-        tokenAmountOwed[address(_Governor)] -= uint(Token_liquidity);
-        tokenAmountDrawn[address(_Governor)] += uint(amount);
-        tokenAmountOwed[address(_Governor)] -= uint(amount);
+        require(uint(amount) <= uint(Token_liquidity),"Overdraft prevention: ERC20");
         IERC20(token).transfer(payable(_Governor), amount);
         success = true;
         assert(success);
